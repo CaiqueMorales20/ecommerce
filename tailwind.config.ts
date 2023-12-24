095cf376bf4742606b41234598d025115540ae77
Caique Morales <caiquemorales20@gmail.com>
2023-12-24 01:58:25 -0300

    :sparkles: feat: Base config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,87 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: '#D87D4A',
+        'primary-300': '#fbaf85',
+        dark: '#101010',
+        darker: '#000000',
+        neutral: '#ffffff',
+        'neutral-500': '#FAFAFA',
+        'neutral-600': '#F1F1F1',
+      },
+      fontSize: {
+        h1: [
+          '56px',
+          {
+            lineHeight: '58px',
+            letterSpacing: '2px',
+            fontWeight: '700',
+          },
+        ],
+        h2: [
+          '40px',
+          {
+            lineHeight: '44px',
+            letterSpacing: '1.5px',
+            fontWeight: '700',
+          },
+        ],
+        h3: [
+          '32px',
+          {
+            lineHeight: '36px',
+            letterSpacing: '1.15px',
+            fontWeight: '700',
+          },
+        ],
+        h4: [
+          '28px',
+          {
+            lineHeight: '38px',
+            letterSpacing: '2px',
+            fontWeight: '700',
+          },
+        ],
+        h5: [
+          '24px',
+          {
+            lineHeight: '33px',
+            letterSpacing: '1.7px',
+            fontWeight: '700',
+          },
+        ],
+        h6: [
+          '18px',
+          {
+            lineHeight: '24px',
+            letterSpacing: '1.3px',
+            fontWeight: '700',
+          },
+        ],
+        overline: [
+          '14px',
+          {
+            lineHeight: '19px',
+            letterSpacing: '10px',
+            fontWeight: '400',
+          },
+        ],
+        subtitle: [
+          '13px',
+          {
+            lineHeight: '25px',
+            letterSpacing: '1px',
+            fontWeight: '700',
+          },
+        ],
+        body: [
+          '15px',
+          {
+            lineHeight: '25px',
+            fontWeight: '500',
+          },
+        ],
       },
     },
   },
